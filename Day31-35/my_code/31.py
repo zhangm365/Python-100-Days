839cd3441b68f8c25a9cb28e63b2d333544d009d
zhangm365 <mzhang2048@gmail.com>
2025-06-03 17:45:54 +0800

    add codes

--- Day31-35/my_code/31.py
+++ Day31-35/my_code/31.py
@@ -2,6 +2,8 @@
 """
 Python 进阶编程
 """
+from concurrent.futures.thread import ThreadPoolExecutor
+from fileinput import filename
 
 # 1. 生成式语法
 prices = {
@@ -302,6 +304,7 @@ print(f'快速排序的结果：{quick_sort(data)}')
 """
 
 def main():
+    print('请输入一个列表：')
     items = list(map(int, input().split()))
     overall = partial = items[0]
     for i in range(1, len(items)):
@@ -309,6 +312,64 @@ def main():
         overall = max(overall, partial)
 
     print(overall)
+
+if __name__ == '__main__':
+    main()
+
+"""
+并发编程
+"""
+
+import glob
+import os
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+from PIL import Image
+
+PREFIX = "thumbnails"
+
+def generate_thumbnail(infile, size, format='PNG'):
+    """生成指定图片的缩略图"""
+    try:
+        # 路径处理
+        filename = os.path.basename(infile)
+        name, ext = os.path.splitext(filename)
+
+        # 输出目录
+        os.makedirs(PREFIX, exist_ok = True)
+        outfile = os.path.join(PREFIX, f'{name}_{size[0]}_{size[1]}{ext}')
+
+        with Image.open(infile) as img:
+            if not (isinstance(size, (tuple, list)) and len(size) == 2):
+                raise ValueError("size 参数必须是包含两个整数的元组")
+
+            img.thumbnail(size, Image.Resampling.LANCZOS)
+            img.save(outfile, format=format)
+
+        return True
+
+    except FileNotFoundError:
+        print(f"错误：文件{infile}不存在")
+        return False
+    except Image.UnidentifiedImageError:
+        print(f"错误：无法识别的图片格式 - {infile}")
+        return False
+    except Exception as e:
+        print(f"处理文件 {infile} 时发生未知错误：{str(e)}")
+        return False
+
+
+def main():
+    """主函数"""
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        tasks = [
+            executor.submit(generate_thumbnail, infile, (size, size))
+            for infile in glob.glob('images/*.png')
+            for size in (32, 64, 128)
+        ]
+
 
 if __name__ == '__main__':
     main()
