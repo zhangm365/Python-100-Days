f430b0d6b74bf380bd85471ea5719da6c0eee6aa
zhangm365 <mzhang2048@gmail.com>
2025-06-15 23:14:57 +0800

    update code

--- Day61-65/my_code/62.py
+++ Day61-65/my_code/62.py
@@ -8,8 +8,9 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://www.github.com/'
+# 设置请求头，模拟浏览器访问
 headers = {
-    'User-Agent': 'Mozilla/5.0'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
 }
 
 resp = requests.get(url, headers=headers)
@@ -25,4 +26,21 @@ if resp.status_code == 200:
 # 获取 google logo 图片并保存.
 resp = requests.get('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
 with open('google.png', 'wb') as file:
-    file.write(resp.content)
+    file.write(resp.content)    # resp.content 返回的是二进制内容
+
+
+# 获取豆瓣电影 Top 250 页面
+print("\n===== 豆瓣电影 Top 250: =====")
+for page in range(1, 11):
+    url = f'https://movie.douban.com/top250?start={(page - 1) * 25}'
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        
+        for item in soup.find_all('div', class_='item'):
+            rank  = item.find('em').text
+            title = item.find('span', class_='title').text
+            print(rank, title)
+    else:
+        print(f'Failed to retrieve page {page}')
+
