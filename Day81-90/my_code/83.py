a34f22df9d825f5414f0c34ec8821287fd64281d
zhangm365 <mzhang2048@gmail.com>
2025-07-06 23:12:56 +0800

    add codes.

--- Day81-90/my_code/83.py
+++ Day81-90/my_code/83.py
@@ -15,4 +15,18 @@ def entropy(y):
     prob = counts / y.size
     return -np.sum(prob * np.log2(prob))
 
+def info_gain(x, y):
+    """
+    计算信息增益
+    :param x: 给定的特征
+    :param y: 数据集的目标值
+    :return: 信息增益
+    """
+
+    values, counts = np.unique(x, return_counts = True)
+    new_entropy = 0
+    for i, value in enumerate(values):
+        prob = counts[i] / x.size
+        new_entropy += prob * entropy(y[x == value])
+    return entropy(y) - new_entropy
 
