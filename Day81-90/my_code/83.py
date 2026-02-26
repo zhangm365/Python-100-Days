b90e99aedc2d4590daf9f37fd4189d3a4885afa7
zhangm365 <mzhang2048@gmail.com>
2026-02-26 10:01:52 +0800

    add 83.py

--- Day81-90/my_code/83.py
+++ Day81-90/my_code/83.py
@@ -2,10 +2,12 @@
 """
 决策树
 """
+from xml.sax.handler import feature_namespace_prefixes
 
 import numpy as np
 from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+
 
 def entropy(y):
     """
@@ -55,3 +57,78 @@ print(f'R(D,A0) = {info_gain_ratio(X_train[:, 0], y_train)}')
 print(f'R(D,A1) = {info_gain_ratio(X_train[:, 0], y_train)}')
 print(f'R(D,A2) = {info_gain_ratio(X_train[:, 0], y_train)}')
 print(f'R(D,A3) = {info_gain_ratio(X_train[:, 0], y_train)}')
+
+
+def gini_index(y):
+    """
+    计算基尼指数
+    :param y: 数据集的目标值
+    :return: 基尼指数
+    """
+    _, counts = np.unique(y, return_counts=True)
+    return 1 - np.sum((counts / y.size) ** 2)
+
+def gini_with_feature(x, y):
+    """
+    计算给定特征后的基尼指数
+    :param x: 给定的特征值
+    :param y: 数据集的目标值
+    :return: 给定特征后的基尼指数
+    """
+    values, counts = np.unique(x, return_counts=True)
+    gini = 0
+    for value in values:
+        prob = x[x == value].size / x.size
+        gini += prob * gini_index(y[x == value])
+    return gini
+
+print("========基尼指数：=========")
+print(f'G(D) = {gini_index(y_train)}')
+print(f'G(D,A0) = {gini_with_feature(X_train[:, 0], y_train)}')
+print(f'G(D,A1) = {gini_with_feature(X_train[:, 1], y_train)}')
+print(f'G(D,A2) = {gini_with_feature(X_train[:, 2], y_train)}')
+print(f'G(D,A3) = {gini_with_feature(X_train[:, 3], y_train)}')
+
+
+from sklearn.tree import DecisionTreeClassifier
+# 创建模型
+model = DecisionTreeClassifier()
+# 训练模型
+model.fit(X_train, y_train)
+# 预测结果
+y_pred = model.predict(X_test)
+
+print("==========决策树模型==========")
+from sklearn.metrics import classification_report
+print(y_test)
+print(y_pred)
+print(classification_report(y_test, y_pred))
+
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
+
+plt.figure(figsize=(12, 10))
+plot_tree(
+    decision_tree=model,    # 决策树模型
+    feature_names=iris.feature_names,    # 特征的名称
+    class_names=iris.target_names,    # 标签的名称
+    filled=True     # 用颜色填充
+)
+plt.show()
+
+print("======随机森林======")
+from sklearn.ensemble import RandomForestClassifier
+
+gs = GridSearchCV(
+    estimator=RandomForestClassifier(n_jobs=-1),
+    param_grid={
+        'n_estimators': [50, 100, 150],
+        'criterion': ['gini', 'entropy'],
+        'max_depth': np.arange(5, 10),
+        'max_features': ['sqrt', 'log2'],
+        'min_samples_split': np.arange(1, 11),
+        'max_leaf_nodes': np.arange(5, 15)
+    },
+    cv=5,
+)
+gs.fit(X_train, y_train)
